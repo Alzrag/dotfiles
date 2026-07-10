a7204290cf6619aa5a0018a9dc0c9d4bb8a03ba4
Alzrag <jediclark14@icloud.com>
2026-07-09 22:20:52 -0600

    the end is soon

--- hypr/scripts/move-window-left.sh
+++ hypr/scripts/move-window-left.sh
@@ -5,7 +5,7 @@ current_ws=$(hyprctl activewindow -j | jq '.workspace.id')
 
 # Decrement if greater than 1
 if [ "$current_ws" -gt 1 ]; then
-  prev_ws=$((current_ws - 1))
+  prev_ws=$((current_ws + 1))
   # 'silent' moves the window without changing the monitor's active workspace
   hyprctl dispatch movetoworkspacesilent "$prev_ws"
 fi
--- hypr/scripts/move-window-right.sh
+++ hypr/scripts/move-window-right.sh
@@ -5,7 +5,7 @@ current_ws=$(hyprctl activewindow -j | jq '.workspace.id')
 
 # Increment if less than 6
 if [ "$current_ws" -lt 6 ]; then
-  next_ws=$((current_ws + 1))
+  next_ws=$((current_ws - 1))
   # 'silent' moves the window without changing the monitor's active workspace
   hyprctl dispatch movetoworkspacesilent "$next_ws"
 fi
